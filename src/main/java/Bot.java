b1234dd437a12c53c473d16d9f8b91aac4b6c208
Malenkov Gleb <yourgitemail@example.com>
2022-02-27 19:34:24 +0200

    Added a Procfile.

---
+++ src/main/java/Bot.java
@@ -0,0 +1,63 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+
+public class Bot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotUsername() {
+        return BotRegister.getName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return BotRegister.getToken();
+    }
+
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        String start = update.getMessage().getText();
+        if(start.equals("/start"))
+            infoStart(update);
+        else
+            ReportsHelper(update);
+
+    }
+
+    public  void infoStart(Update update){
+        SendMessage sendMessageStart = new SendMessage();
+        sendMessageStart.setChatId(update.getMessage().getChatId().toString());
+
+        sendMessageStart.setText(
+                        "Привет я бот, мое имя Винни!"
+                        +"\nЯ родился совершенно недавно, моя версия v0.1"
+                        +"\nПомоги мне стать лучше, для этого ты можешь написать моему создателю @MalenkovaSofya."
+                        +"\n\nСейчас я умею присылать ссылки на полезные отчеты твоих клиентов"
+                        +"\nДля этого просто отправь мне логин клиента и я сделаю все сам."
+        );
+
+
+        try{
+            execute(sendMessageStart);
+        }catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void ReportsHelper(Update update){
+        SendMessage sendMessageReports = new SendMessage();
+        sendMessageReports.setChatId(update.getMessage().getChatId().toString());
+        sendMessageReports.enableMarkdown(true);
+        sendMessageReports.setText(ChangeUrls.urls(update.getMessage().getText()));
+
+        try{
+            execute(sendMessageReports);
+        }catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/BotRegister.java
@@ -0,0 +1,14 @@
+public class BotRegister {
+
+    private static final String NAME ="ReportsHelperBot";
+    private static final String TOKEN ="5257318222:AAH2Y_HMCpzwjitjkFaWJr7am7FAl14O6VM";
+
+    public static String getName(){
+        return NAME;
+    }
+
+    public static String getToken(){
+        return TOKEN;
+    }
+
+}
