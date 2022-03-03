0bd75db376ba8236c2587cdcf6fe284b91158761
Malenkov Gleb <yourgitemail@example.com>
2022-03-03 02:14:07 +0200

    Realise version 1_0

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -66,7 +66,7 @@ public class Bot extends TelegramLongPollingBot {
             SendMessage sendMessage = new SendMessage();
             sendMessage.setChatId(chat_id);
 
-            String nameRepost = update.getCallbackQuery().ge;
+
 
             InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
             AnswerKeybordRepost.answer(update,inlineKeyboardMarkup,login);
